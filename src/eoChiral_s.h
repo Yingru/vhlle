9b58e105cc0dc8f6c796efbdf17d4eda28e8daa5
Yingru <yx59@duke.edu>
2017-07-24 12:59:52 -0400

    From last commit (forget to add eos_hotQCD files)

---
+++ src/eoChiral_s.h
@@ -0,0 +1,16 @@
+class EoS ;
+class EoSaux ;
+class EoSChiral : public EoS
+{
+private:
+	EoSaux *eossmall, *eosbig ;
+public:
+	EoSChiral(void);
+	~EoSChiral(void);
+
+	virtual void eos(double e, double nb, double nq, double ns,
+		double &_T, double &_mub, double &_muq, double &_mus, double &_p) ;
+	virtual double p(double e, double nb, double nq, double ns) ;
+        virtual void gete(double s, double& e, double nb);
+
+};
